9b03de61fdec2e772b0dbd22b20e8615189666bc
Mirko Džudžar <44711172+mirkodzudzar@users.noreply.github.com>
2019-07-25 13:32:41 +0200

    Update index.blade.php

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -12,8 +12,6 @@
     </div>
   @endif
 
-  @if(count($comments) > 0)
-
   <h1>Comments</h1>
 
   <table class="table">
@@ -26,54 +24,54 @@
       </tr>
     </thead>
     <tbody>
-      @foreach($comments as $comment)
-      <tr>
-        <td>{{$comment->id}}</td>
-        <td>{{$comment->author}}</td>
-        <td>{{$comment->email}}</td>
-        <td>{{$comment->body}}</td>
-        <td><a href="{{route('home.post', $comment->post->slug)}}">View post</a></td>
-        <td><a href="{{route('admin.comment.replies.show', $comment->id)}}">View replies</a></td>
-        <td>
+      @forelse($comments as $comment)
+        <tr>
+          <td>{{$comment->id}}</td>
+          <td>{{$comment->author}}</td>
+          <td>{{$comment->email}}</td>
+          <td>{{$comment->body}}</td>
+          <td><a href="{{route('home.post', $comment->post->slug)}}">View post</a></td>
+          <td><a href="{{route('admin.comment.replies.show', $comment->id)}}">View replies</a></td>
+          <td>
 
-          @if($comment->is_active == 1)
-            {!! Form::open(['method' => 'PATCH', 'action' => ['PostCommentsController@update', $comment->id]]) !!}
+            @if($comment->is_active == 1)
+              {!! Form::open(['method' => 'PATCH', 'action' => ['PostCommentsController@update', $comment->id]]) !!}
 
-              <input type="hidden" name="is_active" value="0">
+                <input type="hidden" name="is_active" value="0">
+
+                <div class="form-group">
+                  {!! Form::submit('Un-approve', ['class' => 'btn btn-success']) !!}
+                </div>
+              {!! Form::close() !!}
+            @else
+              {!! Form::open(['method' => 'PATCH', 'action' => ['PostCommentsController@update', $comment->id]]) !!}
+
+                <input type="hidden" name="is_active" value="1">
+
+                <div class="form-group">
+                  {!! Form::submit('Approve', ['class' => 'btn btn-info']) !!}
+                </div>
+              {!! Form::close() !!}
+            @endif
+
+          </td>
+          <td>
+
+            {!! Form::open(['method' => 'DELETE', 'action' => ['PostCommentsController@destroy', $comment->id]]) !!}
 
               <div class="form-group">
-                {!! Form::submit('Un-approve', ['class' => 'btn btn-success']) !!}
+                {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
               </div>
             {!! Form::close() !!}
-          @else
-            {!! Form::open(['method' => 'PATCH', 'action' => ['PostCommentsController@update', $comment->id]]) !!}
 
-              <input type="hidden" name="is_active" value="1">
-
-              <div class="form-group">
-                {!! Form::submit('Approve', ['class' => 'btn btn-info']) !!}
-              </div>
-            {!! Form::close() !!}
-          @endif
-
-        </td>
-        <td>
-
-          {!! Form::open(['method' => 'DELETE', 'action' => ['PostCommentsController@destroy', $comment->id]]) !!}
-
-            <div class="form-group">
-              {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-            </div>
-          {!! Form::close() !!}
-
-        </td>
-      </tr>
-      @endforeach
+          </td>
+        </tr>
+      @empty
+        <tr>
+          <th>No comments found</th>
+        </tr>
+      @endforelse
     </tbody>
   </table>
-
-  @else
-    <h1 class="text-center">No comments</h1>
-  @endif
 
 @endsection
